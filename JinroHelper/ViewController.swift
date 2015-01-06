65e65e64575badfec720992e0a0145e30b2aa52c
Ryo Eguchi <ryo.egu@gmail.com>
2015-01-06 11:06:39 +0900

    Initial Commit

---
+++ JinroHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JinroHelper
+//
+//  Created by Ryo Eguchi on 2015/01/06.
+//  Copyright (c) 2015年 Ryo Eguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JinroHelperTests/JinroHelperTests.swift
@@ -0,0 +1,36 @@
+//
+//  JinroHelperTests.swift
+//  JinroHelperTests
+//
+//  Created by Ryo Eguchi on 2015/01/06.
+//  Copyright (c) 2015年 Ryo Eguchi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class JinroHelperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
